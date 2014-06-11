289618803bdab2e6800fbed7d1956b861a81049f
Kai Fu <kaistuff@yahoo.com>
2014-06-11 17:18:32 +1000

    started users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Lgd::Application.routes.draw do
+  get "users/edit"
   root "static_pages#home"
-  match "/help",  to: "static_pages#help",  via: "get"
-	match "/about", to: "static_pages#about", via: "get"
+  match "/help",   to: "static_pages#help",  via: "get"
+	match "/about",  to: "static_pages#about", via: "get"
+	match "/signup", to: "users#new", 				 via: "get"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
