d15711f7ddd65faee48bb45daa4ec6c2e4f73b42
Kai Fu <kaistuff@yahoo.com>
2014-06-11 17:01:23 +1000

    basic statics

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,39 +1,24 @@
 require 'spec_helper'
 
 describe "Static pages" do
+	subject { page }
   describe "Home page" do
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Legal Guide Dog')
-    end
-		it "should have the right title" do
-			visit '/static_pages/home'
-			expect(page).not_to have_title("| Home")
-		end
+		before { visit root_path }
+		it { should have_content('Legal Guide Dog') }
+		it { should have_title(full_title('')) }
+		it { should_not have_title('| Home')}
   end
 	
   describe "Help page" do
-
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-		it "should have the right title" do
-			visit '/static_pages/help'
-			expect(page).to have_title("Legal Guide Dog | Help")
-		end
+		before { visit help_path }
+		it { should have_content('Help') }
+		it { should have_title(full_title('Help')) }
   end
 	
   describe "About page" do
-
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
-		it "should have the right title" do
-			visit '/static_pages/about'
-			expect(page).to have_title("Legal Guide Dog | About Us")
-		end
+		before { visit about_path }
+		it { should have_content('About Us') }
+		it { should have_title(full_title('About Us')) }
   end
 	
 end
