71412a0f4420fc1d8b2b77cc733b624634a5832f
Kai Fu <kaistuff@yahoo.com>
2014-06-12 12:19:18 +1000

    finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,28 @@
 class UsersController < ApplicationController
   def new
+		@user=User.new
   end
-
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+			flash[:success] = "Welcome to my home, take a look around!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
   def edit
   end
+	
+	def show
+		@user = User.find(params[:id])
+	end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
 	validates :name, presence: true, length: { maximum: 15 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false }
-	before_save { self.email = email.downcase }
+	before_save { email.downcase! }
 	validates :password, length: { minimum: 6 }
 	has_secure_password
 end
